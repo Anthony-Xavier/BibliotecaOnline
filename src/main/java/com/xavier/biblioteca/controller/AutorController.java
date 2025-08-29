0df4aa60b6b4f0a0a9ac6616c57097528bb42c79
Anthony xavier <anthonyltx12@gmail.com>
2025-08-29 10:14:34 -0300

    ajustar AutorService com listarAutores e buscarAutorPorId

--- src/main/java/com/xavier/biblioteca/controller/AutorController.java
+++ src/main/java/com/xavier/biblioteca/controller/AutorController.java
@@ -1,4 +1,42 @@
 package com.xavier.biblioteca.controller;
 
+import com.xavier.biblioteca.model.Autor;
+import com.xavier.biblioteca.model.Livro;
+import com.xavier.biblioteca.service.AutorService;
+import jakarta.annotation.security.PermitAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/autores")
 public class AutorController {
+
+    @Autowired
+    public AutorService autorService;
+
+    // Cadastrar autor
+    @PostMapping
+    public Autor cadastrarAutor(@RequestBody Autor autor){
+        return autorService.cadastrarAutor(autor);
+    }
+
+    // Listar livros de um autor
+    @GetMapping("/{id}/livros")
+    public List<Livro> listarLivrosPorAutor(@PathVariable Long id){
+        return autorService.listarLivrosPorAutor(id);
+    }
+
+    //Listar todos os autores
+    @GetMapping
+    public List<Autor> listarAutores(){
+        return autorService.listarAutores();
+    }
+
+    // Buscar autor por ID
+    @GetMapping("/{id}")
+    public Autor buscarAutorPorId(@PathVariable Long id) {
+        return autorService.buscarAutorPorId(id);
+    }
 }
--- src/main/java/com/xavier/biblioteca/service/AutorService.java
+++ src/main/java/com/xavier/biblioteca/service/AutorService.java
@@ -19,10 +19,21 @@ public class AutorService {
         return autorRepository.save(autor);
     }
 
+    // Listar livros de um autor
     public List<Livro> listarLivrosPorAutor(Long id){
         Autor autor = autorRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Autor não encontrado"));
         return autor.getLivros();
     }
 
+    //Listar todos os autores
+    public List<Autor> listarAutores() {
+        return autorRepository.findAll();
+    }
+
+    // Buscar autor por ID
+    public Autor buscarAutorPorId(Long id) {
+        return autorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Autor não encontrado"));
+    }
 }
